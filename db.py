f0b740e3220d98a1af71d4c0138f5e2ae85c6481
Botashka-Nur <nurkasyvomabota@gmail.com>
2025-04-16 14:04:21 +0500

    lab 10

--- db.py
+++ db.py
@@ -8,3 +8,16 @@ def get_connection():
         password="Botashka07"
     )
     return conn
+
+#SELECT * FROM user_score;
+'''SELECT u.username, s.score, s.level, s.saved_at
+FROM user_score s
+JOIN users u ON u.id = s.user_id;'''
+#SELECT * FROM users;
+
+#SELECT * FROM contacts;
+#SELECT * FROM phonebook;
+#\dt
+#SELECT username FROM phonebook;
+#SELECT phone FROM phonebook;
+
